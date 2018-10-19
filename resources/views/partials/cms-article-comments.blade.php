16e6f2408ceb5161db3eeae46ffc96b96154f2f3
alessandrofuda <alessandro.fuda@gmail.com>
2018-10-19 14:05:16 +0000

    re-writed cms views structure

---
+++ resources/views/partials/cms-article-comments.blade.php
@@ -0,0 +1,135 @@
+<!-- Cms Comments -->
+
+
+<!--notifications-->
+@if(Session::has('success_message')) {{-- variabili di sessione per alerts --}}
+<div id="alert" class="alert alert-success text-center alert-dismissable fade in">
+  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  {!! Session::get('success_message') !!}
+</div>
+@endif
+
+@if(Session::has('error_message')) 
+<div id="alert" class="alert alert-danger text-center alert-dismissable fade in">
+  <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+  {!! Session::get('error_message') !!}
+</div>
+@endif
+
+{{--error validation form --}}
+@if (count($errors) > 0)
+<div class="alert alert-danger">
+  <ul>
+    @foreach ($errors->all() as $error)
+    <li>{{ $error }}</li>
+    @endforeach
+  </ul>
+</div>
+@endif
+
+
+<!--comments list-->
+<div id="comments" class="well">
+  @if (count($comments) > 0)
+  <h4>
+    <strong>{{ $tot }}</strong> comment{{ $tot === 1 ? 'o':'i' }} all'articolo <strong>{{ $comments->first()->post->title }}</strong>
+  </h4>
+  @endif
+
+  <ul class="media-list">
+    <!-- Comments loop -->
+    @foreach ($comments as $comment)
+    <li id="comment-{{ $comment->id }}" class="media">
+      <div class="media-body">
+        <h4 class="media-heading">
+          {{$comment->from_user_name}} <small>- {{ date('d/m/Y H:i', strtotime($comment->created_at)) }}</small>
+        </h4>
+
+        <p class="comment-body"> {{ $comment->body }} </p> {{--VERIFICARE SICUREZZA TAG HTML E VIRGOLETTE --}}
+
+        <!-- reply btn-->
+        <p class="reply text-right">
+          <button class="btn btn-primary btn-sm" data-toggle="collapse" data-target="#respond-comment-{{ $comment->id }}">Rispondi</button>
+        </p>
+
+
+        @foreach ($comments_child as $comment_child) 
+        @if ($comment_child->comment_parent == $comment->id ) {{-- !!! RISOLTOOOO VERIFICARE !!! !!!! --}}
+        <!--Nested Comment - annidamento-->
+        <div class="media reply-comm">
+          <div class="media-body">
+            <h4 class="media-heading">
+              {{ $comment_child->from_user_name }} <small>- {{ date('d/m/Y H:i', strtotime($comment_child->created_at)) }}</small>
+            </h4>
+            <p class="comment-body"> {{ $comment_child->body }} </p>
+          </div>
+        </div>
+        <!--Fine Nested Comment - annidamento-->
+        @endif
+        @endforeach
+
+
+        <!-- comment-reply form -->
+        <div id="respond-comment-{{ $comment->id }}" class="collapse comment-respond">
+          <h4>Rispondi al commento di <b>{{ $comment->from_user_name }}</b></h4>
+
+          <form id="reply-commentform-{{ $comment->id }}" class="" action="{{ url($comment->post->slug . '/comment/send') }}" method="post">
+            {{ csrf_field() }}
+            <div class="form-group">
+              <label>Nome</label> <!--for="c-name"-->
+              <input class="form-control" type="text" name="c-name">
+              <label>Email</label> <!--for="c-email"-->
+              <small>(Il tuo indirizzo email non sarà pubblicato)</small>
+              <input class="form-control" type="email" name="c-email">
+              <label>Commento</label> <!--for="c-body"-->
+              <textarea class="form-control" rows="3" name="c-body"></textarea>
+
+              <input name="c-subscription" type="checkbox" value="1" checked />
+              <label>Voglio ricevere aggiornamenti su questa discussione</label> <!--for="c-subscription"-->
+
+              <!--input name="comment_post_ID" value="1" id="comment_post_ID" type="hidden"-->
+              <input name="comment_parent" id="comment_parent_{{ $comment->id }}" value="{{ $comment->id }}" type="hidden">
+
+            </div>
+            <button type="submit" class="btn btn-primary">Invia</button>
+          </form>
+
+          <div class="text-right">
+            <button class="btn btn-link" data-toggle="collapse" data-target="#respond-comment-{{ $comment->id }}">Comprimi / Annulla</button>
+          </div>
+          
+        </div>
+
+      </div>
+    </li>
+    @endforeach
+
+  </ul>
+
+  <hr>
+  <!-- Comment Form -->
+  <h4>Lascia un commento {{ count($comments) > 0 ? 'anche tu' : '' }}  o fai la tua richiesta:</h4>
+  <form id="commentform" class="" action="{{ url($comment->post->slug . '/comment/send') }}" method="post">
+    {{ csrf_field() }}
+    <div class="form-group">
+      <label>Nome</label> <!--for="c-name"-->
+      <input class="form-control" type="text" name="c-name">
+      <label>Email</label> <!-- for="c-email" -->
+      <small>(Il tuo indirizzo email non sarà pubblicato)</small>
+      <input class="form-control" type="email" name="c-email">
+      <label>Commento</label> <!--for="c-body"-->
+      <textarea class="form-control" rows="3" name="c-body"></textarea>
+      <input type="checkbox" name="c-subscription" value="1" checked>
+      <label>Voglio ricevere aggiornamenti su questa discussione</label> <!--for="c-subscription"-->
+      <input name="comment_parent" id="comment_parent" value="0" type="hidden">
+    </div>
+    <button type="submit" class="btn btn-primary">Invia</button>
+  </form>
+
+  <form id="user-subscr" class="form-user-subscr" action="{{ url($comment->post->slug . '/comment/subscribe') }}" method="post">
+    {{ csrf_field() }}
+    <!--input class="" type="text" name="name-subscr" placeholder="Nome"-->
+    <input class="" type="email" name="mail-subscr" placeholder="e-mail">
+    <button type="submit" class="btn btn-primary btn-sm" >Voglio ricevere aggiornamenti su questa discussione senza lasciare un commento</button>
+  </form>
+</div><!--#comments-->
---
+++ resources/views/partials/cms-article-index.blade.php
@@ -0,0 +1,46 @@
+{{--cms top page index--}}
+<div class="col-md-12">
+  <div id="indice-jumb" class="jumbotron text-center">
+    <button class="btn btn-primary text-uppercase"> Indice <span class="caret"></span></button>
+    <div id="indice">
+      <ul class="list-group">
+        <li class="list-group-item">
+          <a href="#tabella-prezzi">Tabella Riassuntiva Prezzi</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#cosa-e-un-video-citofono">Cosa è un Video Citofono</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#detrazioni-fiscali-50-per-acquisto-installazione-videocitofoni">Le detrazioni fiscali sull'acquisto e installazione dei videocitofoni</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#iva-agevolata-acquisto-videocitofoni">L'Iva agevolata sull'acquisto dei videocitofoni</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#citofono-videocitofono-quale-scegliere">Citofono o Videocitofono, quale scegliere?</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#come-scegliere-videocitofono-adatto">Come scegliere il videocitofono più adatto</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#quali-caratteristiche-deve-avere-un-buon-videocitofono">Quali caratteristiche deve avere un buon videocitofono</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#confrontare-videocitofoni-checklist">Cosa confrontare PRIMA di comprare un videocitofono: una checklist per orientarsi.</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#quanti-tipi-di-videocitofoni-esistono">Quanti tipi di videocitofoni esistono</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#quali-marche-producono-impianti-videocitofonici">Quali sono le marche dei principali produttori</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#videocitofoni-wireless-senza-fili">I Videocitofoni Wireless</a>
+        </li>
+        <li class="list-group-item">
+          <a href="#integrare-videocitofono-con-impianto-di-allarme-domestico">Integrare un videocitofono con un Impianto di Allarme Domestico</a>
+        </li>
+      </ul>
+    </div>  
+  </div><!--#indice-jumb-->
+</div>
