0e82ac0b3c7abc27562bf4801247c4516b510a89
alessandrofuda <alessandro.fuda@gmail.com>
2020-04-28 19:44:05 +0200

    new psw page , css fix

--- resources/views/backend/comparatorChangepswd.blade.php
+++ resources/views/backend/comparatorChangepswd.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.comparator')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+<div class="change-passw-wrap">
+    <div class="change-passw-row">
+        <div class="change-passw">
             <div class="panel panel-default">
                 <div class="panel-heading text-center">Cambio Password</div>
 
