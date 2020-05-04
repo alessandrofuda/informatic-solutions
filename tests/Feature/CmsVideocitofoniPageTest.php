c1d322b53db4bcf9da3a20ed60e67011ac83f7f9
alessandro <alessandro.fuda@gmail.com>
2020-05-04 17:24:49 +0000

    testing & refactor

--- tests/Feature/CmsVideocitofoniPageTest.php
+++ tests/Feature/CmsVideocitofoniPageTest.php
@@ -2,15 +2,33 @@
 
 namespace Tests\Feature;
 
+use App\Post;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\RefreshDatabase; 
 
 class CmsVideocitofoniPageTest extends TestCase {
     
-    public function test_videocitofoni_page_works_correctly() {
+    use RefreshDatabase;
 
-        $response = $this->get('/videocitofoni');
+    public function test_cms_page_works_correctly() {
+
+        $this->withoutExceptionHandling();
+        
+        $post = factory(Post::class)->create(['slug' => 'videocitofoni']);
+        $response = $this->get('videocitofoni');
         $response->assertStatus(200);
     }
+
+    public function test_404_if_page_dont_exist() {
+        $response_404 = $this->get('any-other-slug');
+        $response_404->assertStatus(404);
+    }
+
+    // public function test_article_title_is_unique() {
+    //     $post1 = factory(Post::class)->create(['slug' => 'slug-1']);
+        
+
+    // }
+
 }
